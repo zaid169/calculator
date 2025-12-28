a2f49e8c9587362af1c4dda9233d25f1612873ec
ZAID KHAN <zaidkhann437@gmail.com>
2025-12-28 15:43:27 +0530

    Add files via upload

---
+++ zaid.js
@@ -0,0 +1,17 @@
+let display = document.getElementById("display");
+
+function appendValue(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Error";
+    }
+}
